f336953be7fbfcce7e9e809c9efa4395e438557f
Adelelsayedahmed <74651737+Adelelsayedahmed@users.noreply.github.com>
2023-02-25 13:18:26 +0200

    adding getAvg function

--- main.cpp
+++ main.cpp
@@ -7,9 +7,19 @@ int  getSum(vector <int> v)
     }
     return sum;
 }
+int  getAvg(vector <int> v)
+{
+    int sum = 0;
+    for(int i = 0; i < v.size(); i++)
+    {
+        sum += v[i];
+    }
+    return (sum / v.size());
+}
 int main(void)
 {
     vector <int> v {1,2,3,4,5,6,7,8,9,10};
     cout << getSum(v) << "\n";
+    cout << getAvg(v) << "\n";
     return 0;
 }
